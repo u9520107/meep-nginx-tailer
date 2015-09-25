e687c19b75e20732289c2e26f3c4ad60a1131213
Sunny <sunny@meepshop.com>
2015-09-25 14:26:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ function processLine(data) {
     }
 
     //check for offenders
-    if(status === '499' ||
+    if(status === '444' ||
        checkUrl(data.http_referrer) ||
        checkUa(data.http_user_agent)
       ) {
